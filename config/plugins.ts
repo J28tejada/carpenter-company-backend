523050e8858e828139667e54c708d74b9840d6b3
j28tejada <josuetejadaromero@gmail.com>
2025-11-08 12:00:43 -0400

    feat: wire upload plugin to env vars

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,38 @@
-export default () => ({});
+export default ({ env }) => {
+  const baseUrl = env('STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_BASE_URL');
+  const forcePathStyle = env.bool(
+    'STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_FORCE_PATH_STYLE',
+    true,
+  );
+  const acl = env('STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_ACL', 'public-read');
+
+  return {
+    upload: {
+      config: {
+        provider: env('STRAPI_PLUGIN_UPLOAD_PROVIDER', 'local'),
+        providerOptions: {
+          accessKeyId: env('STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_ACCESS_KEY_ID'),
+          secretAccessKey: env(
+            'STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_SECRET_ACCESS_KEY',
+          ),
+          region: env('STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_REGION'),
+          endpoint: env('STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_ENDPOINT'),
+          baseUrl,
+          forcePathStyle,
+          params: {
+            Bucket: env('STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_BUCKET'),
+          },
+        },
+        actionOptions: {
+          upload: {
+            ACL: acl,
+          },
+          uploadStream: {
+            ACL: acl,
+          },
+          delete: {},
+        },
+      },
+    },
+  };
+};
