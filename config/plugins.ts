a4a35dc0b28cfe06309a5640e2708e10d3231190
j28tejada <josuetejadaromero@gmail.com>
2025-11-08 12:23:32 -0400

    fix: load digitalocean credentials via s3Options

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,8 @@
 export default ({ env }) => {
   const baseUrl = env('STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_BASE_URL');
+  const endpoint = env('STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_ENDPOINT');
+  const region = env('STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_REGION');
+  const bucket = env('STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_BUCKET');
   const forcePathStyle = env.bool(
     'STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_FORCE_PATH_STYLE',
     true,
@@ -11,16 +14,22 @@ export default ({ env }) => {
       config: {
         provider: env('STRAPI_PLUGIN_UPLOAD_PROVIDER', 'local'),
         providerOptions: {
-          accessKeyId: env('STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_ACCESS_KEY_ID'),
-          secretAccessKey: env(
-            'STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_SECRET_ACCESS_KEY',
-          ),
-          region: env('STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_REGION'),
-          endpoint: env('STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_ENDPOINT'),
           baseUrl,
-          forcePathStyle,
+          s3Options: {
+            credentials: {
+              accessKeyId: env(
+                'STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_ACCESS_KEY_ID',
+              ),
+              secretAccessKey: env(
+                'STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_SECRET_ACCESS_KEY',
+              ),
+            },
+            region,
+            endpoint,
+            forcePathStyle,
+          },
           params: {
-            Bucket: env('STRAPI_PLUGIN_UPLOAD_PROVIDER_OPTIONS_BUCKET'),
+            Bucket: bucket,
           },
         },
         actionOptions: {
